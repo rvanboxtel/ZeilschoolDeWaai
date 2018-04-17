0c09df92a50f5a1b47b2cc8891137471905d0ac4
rvanboxtel <rvanboxtel@roc-dev.com>
2018-04-17 08:39:02 +0200

    navbar component

--- ZeilschoolDeWaai/src/app/app-routing.module.ts
+++ ZeilschoolDeWaai/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }];
+const routes: Routes = [{ path: '', component: HomepageComponent, pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ZeilschoolDeWaai/src/app/app.module.ts
+++ ZeilschoolDeWaai/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
