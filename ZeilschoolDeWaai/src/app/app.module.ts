fdda1037761c390e96df697cf11b90437a994d5a
rvanboxtel <rvanboxtel@roc-dev.com>
2018-04-17 10:16:12 +0200

    layout bijna klaar homepage

--- ZeilschoolDeWaai/src/app/app.module.ts
+++ ZeilschoolDeWaai/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { LayoutModule } from '@progress/kendo-angular-layout';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -16,7 +18,10 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    LayoutModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
